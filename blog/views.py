68f08a66ba0f5a617df730bff9ab251683f4397d
baaaam771 <jeongiun@naver.com>
2020-08-03 23:28:46 +0900

    add new blog page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
+from django.utils import timezone
 from .models import Blog
 # Create your views here.
 
@@ -13,3 +14,14 @@ def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk = blog_id)
 
     return render(request, 'detail.html', {'blog' : blog_detail})
+
+def new(request): #new.html을 띄워주는 함수
+    return render(request, 'new.html')
+
+def create(request): #입력 내용 데이터베이스에 넣어줌
+    blog= Blog()
+    blog.title= request.GET['title']
+    blog.body= request.GET['body']
+    blog.pub_date= timezone.datetime.now()
+    blog.save() #데이터베이스에 저장해라
+    return redirect('/blog/'+str(blog.id))
