0d864f4837bb6f6e770e37690bde7bd49d5f1648
baaaam771 <jeongiun@naver.com>
2020-08-13 02:59:27 +0900

    pagination

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, get_object_or_404,redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
 # Create your views here.
 
 def home(request):
     blogs=Blog.objects #쿼리셋 #메소드
-    return render(request, 'home.html', {'blogs': blogs})
+    #블로그 모든 글들을 대상으로
+    blog_list =Blog.objects.all()
+    #블로그 객체 세 개를 한 페이지로 자른다
+    paginator= Paginator(blog_list, 3)
+    #request 받은 페이지가 뭔지 알아낸다(페이지 번호)(request페이지를 변수에 담는다)
+    page= request.GET.get('page')
+    #request된 페이지를 얻어온 뒤 return 해준다
+    posts= paginator.get_page(page)
+    return render(request, 'home.html', {'blogs': blogs, 'posts': posts})
 
     #쿼리셋과 메소드의 형식
     #모델.쿼리셋(objects).메소드
