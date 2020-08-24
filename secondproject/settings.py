9051f67de6ad7e06740ae5f3fba3f88b01c06a5c
baaaam771 <jeongiun@naver.com>
2020-08-24 17:44:02 +0900

    return to sqlite

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -82,12 +82,14 @@ WSGI_APPLICATION = 'secondproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'postgres',
-        'PASSWORD': 'lbg630517',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'blog',
+        #'USER': 'postgres',
+        #'PASSWORD': 'lbg630517',
+        #'HOST': '127.0.0.1',
+        #'PORT': '5432',
     }
 }
 
@@ -142,7 +144,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL ='/media/'
 
-SITE_ID = 1
+#SITE_ID = 1
 #admin 사이트 연결이 안되서 추가
 
 
